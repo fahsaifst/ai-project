c6a537aa03da331ceb578ca11bf3013451ff4612
parnlees <98146361+parnlees@users.noreply.github.com>
2023-11-29 21:42:50 +0700

    fix arai mairu

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import serial
 import supervision as sv 
 from ultralytics import yolov8 as YOLO
 
-from pydub import AudioSegment
-from pydub.playback import play
+#from pydub import AudioSegment
+#from pydub.playback import play
 
 arduino = serial.Serial(port='COM3', baudrate=9600, timeout=.1)
 
@@ -15,13 +15,13 @@ def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='YOLOv8 Object Detection')
     parser.add_argument(
          "--webcam-resolution",
-         defult=[1280, 720],
+         default=[1280, 720],
          type=int,
          nargs=2,)
     args = parser.parse_args()
     return args
 
-# เขียนตรงนี้จ้า
+#เขียนตรงนี้จ้า
 def main(frame):
     classes = ['helmet', 'no_helmet'] # [0, 1]
 
@@ -46,26 +46,25 @@ def main(frame):
         result = model(frame)
         detections = sv.Detection.from_yolov8(result)
         lables = [
-            f'{model.model.names[class_id]} {confidence:0.2f}'
-            for _, confidence, class_id, 
-            in detections
+            f'{model.model.names[classid]} {confidence:0.2f}'
+            for _, confidence, class_id in detections
         ]
         frame = box_annotation.annotate(
             scene = frame, 
             detections = detections,
             lables = lables
         )
-       # if lables == 'no_helmet':
+       #if lables == 'no_helmet':
        # cv2.imshow('not_helmet', frame)
+
+        if any(classid == 1 for , _, classid,  in detections):
+            detected_class = "no_helmet"
+            print("Warning! Rider or passenger doesn't wearing HELMET!")
+            print("Detected Class: ", detected_class)
 
         if not ret:
             break
         no_helmet_detected = main(frame)
-
-        if any(class_id == 1 for _, _, class_id, _ in detections):
-            detected_class = "no_helmet"
-            print("Warning! Rider or passenger doesn't wearing HELMET!")
-            print("Detected Class: ", detected_class)
 
     cap.release()
     cv2.destroyAllWindows()
