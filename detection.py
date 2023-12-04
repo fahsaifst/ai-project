5d1cf01da4945d66d6cd09bdfb6fd210f5f66464
fahsaifst <98171729+fahsaifst@users.noreply.github.com>
2023-12-04 09:32:05 +0700

    [LASTEST] final lastest update

--- detection.py
+++ detection.py
@@ -4,14 +4,14 @@ import cv2
 import glob
 
 def detect_helmet():
-    model = YOLO('/Users/fahsaifst/Desktop/ultralytics/model/best_v9.pt') #lastest model is best_v9
+    model = YOLO('/Users/fahsaifst/Desktop/ai-project/best_v9.pt') #lastest model is best_v9
     path_files = glob.glob('detect_test/*')
     for path_file in path_files:
         #source from imgs and videos folder
         #results = model.predict(source=path_file, show=True)
         
         #source from partucular img and video folder
-        results = model.predict(source='', show=True)
+        #results = model.predict(source='', show=True)
         
         #source from default webcam
         results = model.predict(source="0", show=True)
